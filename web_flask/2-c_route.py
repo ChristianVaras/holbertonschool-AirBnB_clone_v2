5e847cf9d8475fb58f1d77437e31306f07b60777
Christian Varas <christianvaras.2020@gmail.com>
2022-09-11 15:12:47 -0500

    Display C followed by the value of the text variable

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+script starts Flask web app
+"""
+
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.route('/')
+def index_hbnb():
+    """display text"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def path_hbnb():
+    """display text"""
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def path_ctext(text):
+    """display custom text"""
+    return "C {}".format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
