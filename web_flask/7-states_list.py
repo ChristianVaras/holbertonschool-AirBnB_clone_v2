00ee857ab55baea88ab34a44939066e2bb7070c7
Christian Varas <christianvaras.2020@gmail.com>
2022-09-14 23:04:26 -0500

    Add script for flask web app that displays html with db data

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+script starts Flask web app
+"""
+from models import storage
+from flask import Flask, render_template
+from models.state import State
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/states_list')
+def states_list():
+    """html that shows the list of states"""
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def close_db(self):
+    """Close SLQAlchemy session"""
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000", debug=True)
